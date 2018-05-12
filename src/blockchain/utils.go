0f84521e36adc3d3ae8e6d503c46841e6ab59c7c
aeshes <mathesis@inbox.ru>
2018-05-12 14:38:09 +0300

    proof of work

---
+++ src/blockchain/utils.go
@@ -0,0 +1,15 @@
+package main
+
+import "bytes"
+import "encoding/binary"
+import "log"
+
+func IntToHex(num int64) []byte {
+    buff := new(bytes.Buffer)
+    err := binary.Write(buff, binary.BigEndian, num)
+    if err != nil {
+        log.Panic(err)
+    }
+
+    return buff.Bytes()
+}
