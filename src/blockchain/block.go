5373925340630bd17d9a1f83f4c3280e4f732dc7
aeshes <mathesis@inbox.ru>
2018-05-12 15:06:21 +0300

    run mining

--- src/blockchain/block.go
+++ src/blockchain/block.go
@@ -1,28 +1,23 @@
 package main
 
 import "time"
-import "strconv"
-import "bytes"
-import "crypto/sha256"
 
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-}
-
-func (block *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(block.Timestamp, 10))
-	headers := bytes.Join([][]byte{ block.PrevBlockHash, block.Data, timestamp }, []byte{})
-	hash := sha256.Sum256(headers)
-
-	block.Hash = hash[:]
+	Nonce         int
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{ time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash();
+	block := &Block{ time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	proof := NewProofOfWork(block)
+	nonce, hash := proof.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
