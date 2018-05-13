115384914ca2a503d965d694b5526bdee93049df
aeshes <mathesis@inbox.ru>
2018-05-14 00:37:58 +0300

    Serialize/Deserialize block

--- src/blockchain/block.go
+++ src/blockchain/block.go
@@ -1,6 +1,9 @@
 package main
 
 import "time"
+import "bytes"
+import "encoding/gob"
+import "log"
 
 type Block struct {
 	Timestamp     int64
@@ -23,4 +26,28 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+func (block *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
